3b0ff2c5b4353b5108a2b7d60c5cc0738074adbe
Oriol Vila Masip <oriol.vila@student.ie.edu>
2021-06-09 14:56:43 +0200

    Adding black

--- src/ie_utils/__init__.py
+++ src/ie_utils/__init__.py
@@ -1,15 +1,17 @@
-#import pandas as pd
+# import pandas as pd
 
 """
 IE Titanic utils.
 """
 
-__version__ ="0.1.0"
+__version__ = "0.1.0"
 
 import pandas as pd
 
+
 def tokenize(text):
     return text.split()
+
 
 if __name__ == "__main__":
     print(tokenize("Hello World!"))
