7121d0c36a56cdd53ecf5b4dab4da40c14f6b1b2
Oriol Vila Masip <oriol.vila@student.ie.edu>
2021-06-11 14:59:30 +0200

    Using click library

--- src/ie_utils/__main__.py
+++ src/ie_utils/__main__.py
@@ -1,12 +1,25 @@
 from ie_utils import tokenize
-
 import sys
+import click
 
-def main():
-    print(tokenize(sys.argv[1]))
+@click.command()
+@click.option('--sentence',prompt='Write your sentence', default='Hello World')
+
+def main(sentence):
+    """IE Tokenizer"""
+    print(tokenize(sentence))
+
 
 if __name__ == "__main__":
     print(main())
+
+    
+    
+
+#def main():
+#    print(tokenize(sys.argv[1]))
+
+
 
 #print(tokenize("Hello World!"))
 
