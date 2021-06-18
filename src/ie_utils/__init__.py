784badd5f3e485c3fe0372924f2460549f46a9f1
Oriol Vila Masip <oriol.vila@student.ie.edu>
2021-06-18 19:42:32 +0200

    Adding more parameters to tokenize + its tests

--- src/ie_utils/__init__.py
+++ src/ie_utils/__init__.py
@@ -9,9 +9,18 @@ __version__ = "0.1.0"
 import pandas as pd
 
 
-def tokenize(text, lower=False):
+def tokenize(text, lower=False, remove_stopwords=False, remove_punctuation=False):
     if lower:
         text=text.lower()
+    if remove_stopwords:
+        stopwords = ["a", "the", "or", "and"]
+        words= text.split()
+        non_stopwords = [w for w in words if not w.lower() in stopwords]
+        text = " ".join(non_stopwords)             
+    if remove_punctuation:
+        punctuation = [".", ",", "!"]
+        for p in punctuation:
+            text = text.replace(p, '')
     return text.split()
 
 
--- tests/test_ie_utils.py
+++ tests/test_ie_utils.py
@@ -1,4 +1,6 @@
 import pytest
+import pandas as pd
+from numpy.testing import assert_array_almost_equal
 
 from ie_utils import tokenize
 
@@ -32,3 +34,30 @@ def test_tokenize_lower_returns_lowercase_tokens():
     tokens = tokenize(sentence, lower=True)
     
     assert tokens == expected_returns
+    
+    
+def test_series_are_approximately_equal():
+    ser = pd.Series([0.1,0.2, 0.1+0.2])
+    expected_ser= pd.Series([0.1,0.2, 0.3])
+    
+    assert_array_almost_equal(ser, expected_ser)
+    assert 0.1 + 0.2 == pytest.approx(0.3)
+    
+    
+def test_tokenize_remove_stopwords():
+    sentence = "I have a dog"
+    expected_tokens = ["I", "have", "dog"]
+    
+    tokens = tokenize(sentence, remove_stopwords=True)
+    assert tokens == expected_tokens
+    
+def test_tokenize_remove_punctuaction():
+    sentence = "I found your .dog!!"
+    expected_tokens = ["I", "found", "your", "dog"]
+    
+    tokens = tokenize(sentence, remove_punctuation=True)
+    assert tokens == expected_tokens
+    
+
+
+    
