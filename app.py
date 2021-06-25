f2ab07eb2c5dfaa491b0abddd06bb06d1199b55a
Oriol Vila Masip <oriol.vila@student.ie.edu>
2021-06-25 11:22:10 +0200

    Add version in app.py

--- app.py
+++ app.py
@@ -5,13 +5,17 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello World!"
+    return {
+        "message":"Hello World!",
+        "version":"0.1",
+    }
+    
 
 @app.route("/tokenize")
 def do_tokenize():
     print(request.args)
     sentence = request.args["sentence"]
-    lower = (request.args.get("lower", False)
+    lower = request.args.get("lower", False)
     return str(tokenize(sentence, lower=lower))
 
 
